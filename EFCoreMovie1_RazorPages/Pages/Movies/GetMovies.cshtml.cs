9c3402dc84e8a08c0c3245a2d6634bae100480a3
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-10-27 13:13:45 +0200

    Add project files.

---
+++ EFCoreMovie1_RazorPages/Pages/Movies/GetMovies.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EFCoreMovie1_RazorPages.Models;
+
+namespace EFCoreMovie1_RazorPages
+{
+    public class GetMoviesModel : PageModel
+    {
+        public string FilterCriteria { get; set; }
+
+        private MovieDB2Context context;
+        public GetMoviesModel(MovieDB2Context service)
+        {
+            context = service;
+        }
+        public IEnumerable<Movie> Movies { get; set; } = new List<Movie>();
+        public void OnGet()
+        {
+            Movies = context.Movies;
+        }
+    }
+}
---
+++ EFCoreMovie1_RazorPages/Pages/Studios/GetStudios.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EFCoreMovie1_RazorPages.Models;
+
+namespace EFCoreMovie1_RazorPages.Pages.Studios
+{
+    public class GetStudiosModel : PageModel
+    {
+        public string FilterCriteria { get; set; }
+       
+        
+        // Please Uncomment the code below
+
+       private MovieDB2Context context;
+        public GetStudiosModel(MovieDB2Context service)
+        {
+            context = service;
+        }
+        public IEnumerable<Studio> Studios { get; set; } = new List<Studio>();
+        public void OnGet()
+        {
+            Studios = context.Studios;
+        }
+
+    }
+}
